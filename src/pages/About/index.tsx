d1bef0c8a6490be997f0d3e257be0bbaeeaa01a5
张文光 <wenguangzhang@caixin.com>
2020-11-25 17:43:03 +0800

    fix issue-101

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -8,7 +8,7 @@ export default function About() {
       <section className="about">
         <h1 className="about-title">关于</h1>
         <div className="about-content">
-          <p className="about-content-txt">关于我，是一个XXX，搭建此博客只为了巩固计算机知识，有任何建议或者问题都可以联系我</p>
+          <p className="about-content-txt">关于我，是一个XXX，搭建此博客只为了巩固计算机知识，有任何建议或者问题都可以联系我。</p>
           <p>联系方式：</p>
           <ul className="about-content-way">
             <li>Github: {GITHUB_LINK}</li>
